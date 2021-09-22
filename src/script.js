dc0261524af34932bd9408e44396b12f1e482502
Mateusz Łątka <mateusz.latka9@gmail.com>
2021-09-23 01:51:24 +0200

    implement mobile and desktop layout

--- src/script.js
+++ src/script.js
@@ -1,40 +1,34 @@
-console.log("Hello world");
+const faqQuestionTopBoxes = document.querySelectorAll('.faq-question-box-top');
 
-const questionTopBoxes = document.querySelectorAll('.faq-question-box-top');
+faqQuestionTopBoxes.forEach(box => box.addEventListener('click', function (event) {
 
-questionTopBoxes.forEach(box=>box.addEventListener('click',function(event){
-    
     const questionBox = event.target.closest('.faq-question-box');
-    if(!questionBox) return;
-    
+    if (!questionBox) return;
+
     const answerMarginTop = Number.parseFloat(window.getComputedStyle(questionBox.querySelector('.faq-answer')).marginTop);
     const questionMarginTop = Number.parseFloat(window.getComputedStyle(questionBox.querySelector('.faq-question-box-top')).marginTop);
-
     const answerHeight = questionBox.querySelector('.faq-answer').offsetHeight;
     const questionHeight = questionBox.querySelector('.faq-question-box-top').offsetHeight;
-    
-    let openHeight = answerHeight + questionHeight + answerMarginTop * 2 + questionMarginTop * 2;
 
-    console.log(openHeight);
+    let newheight = answerHeight + questionHeight + answerMarginTop * 2 + questionMarginTop * 2;
 
-    if(questionBox.dataset.hidden === 'true'){
+    if (questionBox.dataset.hidden === 'true') {
+        questionBox.style.height = newheight + 'px';
         questionBox.dataset.hidden = 'false';
-        questionBox.style.height = openHeight + 'px';
     } else {
-        questionBox.dataset.hidden = 'true';
         questionBox.style.height = '';
+        questionBox.dataset.hidden = 'true';
     }
 }));
 
-document.querySelector('#contact-form').addEventListener('submit',function(event){
+document.querySelector('#contact-form').addEventListener('submit', function (event) {
     event.preventDefault();
     const email = event.target["email-input"].value;
-    if(validateEmail(email)){
+    if (validateEmail(email)) {
         console.log('good email');
         event.target.dataset.error = 'false';
         return;
     }
-    console.log(event.target)
     event.target.dataset.error = 'true';
 });
 
@@ -42,3 +36,108 @@ function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
+document.querySelector('.features-box ul').addEventListener('click', switchFeature);
+const featuresArray = [
+    {
+        "imageUrl": "./resources/illustration-features-tab-1.svg",
+        "header": "Bookmark in one click",
+        "paragraph": "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.",
+    },
+    {
+        "imageUrl": "./resources/illustration-features-tab-2.svg",
+        "header": "Intelligent search",
+        "paragraph": "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
+    },
+    {
+        "imageUrl": "./resources/illustration-features-tab-3.svg",
+        "header": "Share your bookmarks",
+        "paragraph": "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
+    },
+
+];
+
+const featuresMark = document.querySelector('.mark');
+function switchFeature(event) {
+    if (event.target.tagName !== 'BUTTON') return;
+
+    let currentFeatureNumber = this.dataset.currentFeatureNumber
+    let nextFeatureNumber = event.target.dataset.featureNumber;
+    if (currentFeatureNumber === nextFeatureNumber) return;
+
+    this.dataset.currentFeatureNumber = nextFeatureNumber;
+    featuresMark.style.left = `${(nextFeatureNumber - 1) * 33.3}%`;
+
+
+    const image = document.querySelector('.features-box-bottom img');
+    const header = document.querySelector('.features-box-bottom-side h2');
+    const paragraph = document.querySelector('.features-box-bottom-side p');
+
+    image.style.transform = 'translateX(-10rem)';
+    image.style.opacity = '0';
+
+    header.style.transform = 'translateY(-5rem)';
+    header.style.opacity = '0';
+
+    paragraph.style.transform = 'translateY(-5rem)';
+    paragraph.style.opacity = '0';
+
+    image.addEventListener('transitionend', switchImages);
+
+    function switchImages(event) {
+        this.removeEventListener('transitionend', switchImages);
+
+        image.src = featuresArray[nextFeatureNumber - 1].imageUrl;
+        header.textContent = featuresArray[nextFeatureNumber - 1].header;
+        paragraph.textContent = featuresArray[nextFeatureNumber - 1].paragraph;
+        
+        image.style.opacity = '';
+        image.style.transform = '';
+
+        header.style.opacity = '';
+        header.style.transform = '';
+
+        paragraph.style.opacity = '';
+        paragraph.style.transform = '';
+    }
+}
+
+const mobileNavToggleButtons = document.querySelectorAll('.hamburger-menu, .mobile-nav-close');
+mobileNavToggleButtons.forEach(button => button.addEventListener('click', toggleMobileMenu));
+const mobileNav = document.querySelector('.mobile-nav-box');
+
+function toggleMobileMenu() {
+    console.log(mobileNav.dataset.mobileNav);
+
+    if (mobileNav.dataset.mobileNav === "open") {
+        hideMobileNav();
+    } else {
+        showMobileNav();
+    }
+}
+
+function hideMobileNav() {
+    mobileNav.dataset.mobileNav = "hidden";
+    document.querySelector('.hamburger-menu').style.opacity = '';
+    document.querySelector('nav > .logo').style.opacity = '';
+    document.body.style.overflow = "";
+}
+
+function showMobileNav() {
+    mobileNav.dataset.mobileNav = "open";
+    document.querySelector('.hamburger-menu').style.opacity = '0';
+    document.querySelector('nav > .logo').style.opacity = '0';
+    document.body.style.overflow = "hidden";
+}
+
+window.addEventListener('resize', function () {
+    if (document.documentElement.clientWidth > 633) {
+        hideMobileNav();
+    }
+});
+
+document.querySelectorAll('.mobile-nav-box li, .mobile-nav-box .socials-box > i')
+    .forEach(button => button.addEventListener('click', function () {
+
+        hideMobileNav();
+    }));
