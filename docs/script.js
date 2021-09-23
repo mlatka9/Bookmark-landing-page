f4c8ef33d63998ceea70cb107fd1e085a39c08c7
Mateusz Łątka <mateusz.latka9@gmail.com>
2021-09-23 11:24:24 +0200

    script clean up

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,6 @@
 const faqQuestionTopBoxes = document.querySelectorAll('.faq-question-box-top');
 
 faqQuestionTopBoxes.forEach(box => box.addEventListener('click', function (event) {
-
     const questionBox = event.target.closest('.faq-question-box');
     if (!questionBox) return;
 
@@ -38,6 +37,48 @@ function validateEmail(email) {
 }
 
 document.querySelector('.features-box ul').addEventListener('click', switchFeature);
+
+function switchFeature(event) {
+    if (event.target.tagName !== 'BUTTON') return;
+
+    let currentFeatureNumber = this.dataset.currentFeatureNumber
+    let nextFeatureNumber = event.target.dataset.featureNumber;
+    if (currentFeatureNumber === nextFeatureNumber) return;
+
+    const featuresMark = document.querySelector('.mark');
+    this.dataset.currentFeatureNumber = nextFeatureNumber;
+    featuresMark.style.left = `${(nextFeatureNumber - 1) * 33.3}%`;
+
+    const image = document.querySelector('.features-box-bottom img');
+    const header = document.querySelector('.features-box-bottom-side h2');
+    const paragraph = document.querySelector('.features-box-bottom-side p');
+
+    image.style.transform = 'translateX(-10rem)';
+    image.style.opacity = '0';
+    header.style.transform = 'translateY(-5rem)';
+    header.style.opacity = '0';
+    paragraph.style.transform = 'translateY(-5rem)';
+    paragraph.style.opacity = '0';
+
+    image.addEventListener('transitionend', switchImages);
+    function switchImages() {
+        this.removeEventListener('transitionend', switchImages);
+
+        image.src = featuresArray[nextFeatureNumber - 1].imageUrl;
+        header.textContent = featuresArray[nextFeatureNumber - 1].header;
+        paragraph.textContent = featuresArray[nextFeatureNumber - 1].paragraph;
+
+        image.style.opacity = '';
+        image.style.transform = '';
+
+        header.style.opacity = '';
+        header.style.transform = '';
+
+        paragraph.style.opacity = '';
+        paragraph.style.transform = '';
+    }
+}
+
 const featuresArray = [
     {
         "imageUrl": "./resources/illustration-features-tab-1.svg",
@@ -54,61 +95,14 @@ const featuresArray = [
         "header": "Share your bookmarks",
         "paragraph": "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
     },
-
 ];
 
-const featuresMark = document.querySelector('.mark');
-function switchFeature(event) {
-    if (event.target.tagName !== 'BUTTON') return;
 
-    let currentFeatureNumber = this.dataset.currentFeatureNumber
-    let nextFeatureNumber = event.target.dataset.featureNumber;
-    if (currentFeatureNumber === nextFeatureNumber) return;
-
-    this.dataset.currentFeatureNumber = nextFeatureNumber;
-    featuresMark.style.left = `${(nextFeatureNumber - 1) * 33.3}%`;
-
-
-    const image = document.querySelector('.features-box-bottom img');
-    const header = document.querySelector('.features-box-bottom-side h2');
-    const paragraph = document.querySelector('.features-box-bottom-side p');
-
-    image.style.transform = 'translateX(-10rem)';
-    image.style.opacity = '0';
-
-    header.style.transform = 'translateY(-5rem)';
-    header.style.opacity = '0';
-
-    paragraph.style.transform = 'translateY(-5rem)';
-    paragraph.style.opacity = '0';
-
-    image.addEventListener('transitionend', switchImages);
-
-    function switchImages(event) {
-        this.removeEventListener('transitionend', switchImages);
-
-        image.src = featuresArray[nextFeatureNumber - 1].imageUrl;
-        header.textContent = featuresArray[nextFeatureNumber - 1].header;
-        paragraph.textContent = featuresArray[nextFeatureNumber - 1].paragraph;
-        
-        image.style.opacity = '';
-        image.style.transform = '';
-
-        header.style.opacity = '';
-        header.style.transform = '';
-
-        paragraph.style.opacity = '';
-        paragraph.style.transform = '';
-    }
-}
-
+const mobileNav = document.querySelector('.mobile-nav-box');
 const mobileNavToggleButtons = document.querySelectorAll('.hamburger-menu, .mobile-nav-close');
 mobileNavToggleButtons.forEach(button => button.addEventListener('click', toggleMobileMenu));
-const mobileNav = document.querySelector('.mobile-nav-box');
 
 function toggleMobileMenu() {
-    console.log(mobileNav.dataset.mobileNav);
-
     if (mobileNav.dataset.mobileNav === "open") {
         hideMobileNav();
     } else {
